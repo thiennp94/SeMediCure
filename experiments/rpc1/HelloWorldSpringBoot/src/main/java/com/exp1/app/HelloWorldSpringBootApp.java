5d4a50e0b8288dfd84eaa753d97b9fa478684cf7
rpc1 <rpc1@iastate.edu>
2022-02-08 23:23:10 -0600

    hello world completed

---
+++ experiments/rpc1/HelloWorldSpringBoot/src/main/java/com/exp1/app/HelloWorldSpringBootApp.java
@@ -0,0 +1,14 @@
+package com.exp1.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringBootApp {
+	
+	public static void main(String args[])
+	{
+		SpringApplication.run(HelloWorldSpringBootApp.class, args);
+	}
+
+}
---
+++ experiments/rpc1/HelloWorldSpringBoot/src/main/java/com/exp1/app/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.exp1.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping(value="/")
+	public String hello() {
+		return "Hello World!!";
+	}
+}
