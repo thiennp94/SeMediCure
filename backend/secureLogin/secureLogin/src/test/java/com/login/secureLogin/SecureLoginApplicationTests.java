a9048d3fd20bd96fc9d9a7dc88d5b90a759d0825
rpc1 <rpc1@iastate.edu>
2022-02-18 19:57:56 -0600

    created secureLogin testing proj

---
+++ backend/secureLogin/secureLogin/src/test/java/com/login/secureLogin/SecureLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.secureLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
