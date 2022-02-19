8ecf338dc1cb65d926df4ef3c7ddbb408ff30c06
ptnguyen <ptnguyen@iastate.edu>
2022-02-18 22:33:44 -0600

    Create Initial Client Information

---
+++ frontend/semedicure/app/src/main/java/com/example/semedicure/Client_Info.java
@@ -0,0 +1,14 @@
+package com.example.semedicure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Client_Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_info);
+    }
+}
