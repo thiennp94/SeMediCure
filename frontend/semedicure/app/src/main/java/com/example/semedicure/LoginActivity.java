3e560e05027caa9b7013b9a9fec815cda2fd601e
Josue Torres <jatorres@iastate.edu>
2022-02-18 19:29:57 -0600

    added login screen

--- frontend/semedicure/app/src/main/java/com/example/semedicure/LoginActivity.java
+++ frontend/semedicure/app/src/main/java/com/example/semedicure/LoginActivity.java
@@ -4,19 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void returningUser(View view) {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_login);
     }
 }
