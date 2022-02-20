ff0b04f53fa944ef154e493b511f49d3e6da07de
Josue Torres <jatorres@iastate.edu>
2022-02-20 14:07:12 -0600

    added mock server url for testing purposes

--- frontend/semedicure/app/src/main/java/com/example/semedicure/LoginActivity.java
+++ frontend/semedicure/app/src/main/java/com/example/semedicure/LoginActivity.java
@@ -28,12 +28,12 @@ public class LoginActivity extends AppCompatActivity {
     }
     private void requestString() {
         RequestQueue rQueue = Volley.newRequestQueue(this);
-        String url = "";
+        String url = "https://8be4f6a4-40fe-40de-8be1-e9c3df6a16f7.mock.pstmn.io/Object/";
 
         StringRequest sRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                email = response;
+                email = response.toString();
                 Toast.makeText(LoginActivity.this, email, Toast.LENGTH_SHORT).show();
             }
         }, new Response.ErrorListener() {
