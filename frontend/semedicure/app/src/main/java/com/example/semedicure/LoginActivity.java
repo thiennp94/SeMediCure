ed78bd7c44389d2585073bfe556ecce56c989eac
Josue Torres <jatorres@iastate.edu>
2022-02-20 13:00:23 -0600

    added method in login activity to send an HTTP request

--- frontend/semedicure/app/src/main/java/com/example/semedicure/LoginActivity.java
+++ frontend/semedicure/app/src/main/java/com/example/semedicure/LoginActivity.java
@@ -4,14 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private String email;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    private void requestString() {
+        RequestQueue rQueue = Volley.newRequestQueue(this);
+        String url = "";
+
+        StringRequest sRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                email = response;
+                Toast.makeText(LoginActivity.this, email, Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(LoginActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        rQueue.add(sRequest);
+
+    }
+    public void login(View view){
+        requestString();
     }
 }
