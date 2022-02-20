6d0f261dbc71df78f44d069f7ca21a66d7a923ca
rpc1 <rpc1@iastate.edu>
2022-02-20 11:59:14 -0600

    continuing with the h2 database

--- backend/h2/src/main/java/com/semedicure/h2/services/loginService.java
+++ backend/h2/src/main/java/com/semedicure/h2/services/loginService.java
@@ -1,5 +1,19 @@
 package com.semedicure.h2.services;
 
-public class loginService {
+import org.hibernate.mapping.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.semedicure.h2.models.login;
+import com.semedicure.h2.repositories.loginRepository;
+
+@Service
+public class loginService 
+{
+	@Autowired
+	private static loginRepository LR;
+	
+	public static java.util.List<login> list() {
+		return LR.findAll();
+	}
 }
