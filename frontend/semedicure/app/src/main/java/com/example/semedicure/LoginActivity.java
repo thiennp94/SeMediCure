23211150651e70eca508f2db0b14c407f747363b
Josue Torres <jatorres@iastate.edu>
2022-03-02 01:52:59 -0600

    fixed minor errors in code and added a forgot password screen

--- frontend/semedicure/app/src/main/java/com/example/semedicure/LoginActivity.java
+++ frontend/semedicure/app/src/main/java/com/example/semedicure/LoginActivity.java
@@ -2,12 +2,9 @@ package com.example.semedicure;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AuthenticationRequiredException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -15,11 +12,8 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,11 +25,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
-    private void requestJson() {
+    private void requestString() {
         RequestQueue rQueue = Volley.newRequestQueue(this);
         String url = "https://8be4f6a4-40fe-40de-8be1-e9c3df6a16f7.mock.pstmn.io/Object/";
 
-        StringRequest strRequest = new strRequest(Request.Method.POST, url, new Response.Listener<String>() {
+        StringRequest strRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 Toast.makeText(LoginActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
@@ -56,17 +50,17 @@ public class LoginActivity extends AppCompatActivity {
            }
         };
 
-        rQueue.add(jsonRequest);
+        rQueue.add(strRequest);
 
     }
 
     public void login(View view){
-        requestJson();
+        requestString();
     }
 
     public void forgotPassword(View view){
-//        Intent intent = new Intent(this, ForgotActivity.class);
- //       startActivity(intent);
+        Intent intent = new Intent(this, ForgotPassActivity.class);
+        startActivity(intent);
 
     }
 
