3dfb34642c4bcf2ee35c683a6530c9b1b87547b9
ptnguyen <ptnguyen@iastate.edu>
2022-03-30 23:41:28 -0500

    Update Clinic to Provider Information

--- frontend/semedicure/app/src/main/java/com/example/semedicure/ProviderProfileActivity.java
+++ frontend/semedicure/app/src/main/java/com/example/semedicure/ProviderProfileActivity.java
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -19,7 +17,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class ClinicProfileActivity extends AppCompatActivity {
+public class ProviderProfileActivity extends AppCompatActivity {
 
     private TextView mTextViewClinicName;
     private TextView mTextViewPhone;
@@ -33,11 +31,11 @@ public class ClinicProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_clinic_profile);
+        setContentView(R.layout.activity_provider_profile);
 
-        mTextViewClinicName = findViewById(R.id.txtClinicName);
+        mTextViewClinicName = findViewById(R.id.txtProviderName);
         mTextViewPhone = findViewById(R.id.txtPhone);
-        mTextViewEmail = findViewById(R.id.txtClinicEmail);
+        mTextViewEmail = findViewById(R.id.txtProviderEmail);
         mTextViewAddr = findViewById(R.id.txtStAddr);
         mTextViewCity = findViewById(R.id.txtCity);
         mTextViewZip = findViewById(R.id.txtZip);
