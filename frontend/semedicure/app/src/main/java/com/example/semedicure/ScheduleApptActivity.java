d424170d7a5c65ac041e82f4cfb229607870300a
Josue Torres <jatorres@iastate.edu>
2022-04-02 20:42:08 -0500

    added time selector

--- frontend/semedicure/app/src/main/java/com/example/semedicure/ScheduleApptActivity.java
+++ frontend/semedicure/app/src/main/java/com/example/semedicure/ScheduleApptActivity.java
@@ -2,13 +2,40 @@ package com.example.semedicure;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class ScheduleApptActivity extends AppCompatActivity {
+
+    private CalendarView calendar;
+    private Button timeButton;
+    private int hour, minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_appt);
+
+        calendar = findViewById(R.id.calendarView);
+        timeButton = findViewById((R.id.button7));
+    }
+    public void selectTime(View view) {
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selHr, int selMin) {
+                hour = selHr;
+                minute = selMin; 
+                timeButton.setText(String.format(Locale.getDefault(), "%02d, %02d", selHr, selMin));
+            }
+        };
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, onTimeSetListener, hour, minute, false);
+        timePickerDialog.show();
     }
 }
