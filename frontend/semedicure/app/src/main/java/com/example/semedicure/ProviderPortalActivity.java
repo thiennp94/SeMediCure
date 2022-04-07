aee589f3a3df8913cf8a22a4c2b48cc062e33f9b
ptnguyen <ptnguyen@iastate.edu>
2022-04-06 22:32:14 -0500

    Add Find Patient and Order Prescription screen

--- frontend/semedicure/app/src/main/java/com/example/semedicure/ProviderPortalActivity.java
+++ frontend/semedicure/app/src/main/java/com/example/semedicure/ProviderPortalActivity.java
@@ -2,7 +2,9 @@ package com.example.semedicure;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProviderPortalActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class ProviderPortalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider_portal);
+    }
+
+    public void findPatient(View view) {
+        Intent intent = new Intent(this, FindPatientActivity.class);
+        startActivity(intent);
+    }
+
+    public void orderPrescription(View view) {
+        Intent intent = new Intent(this, OrderPrescriptionActivity.class);
+        startActivity(intent);
+    }
+
+    public void updateProfile(View view) {
+        Intent intent = new Intent(this, ProviderInfoActivity.class);
+        startActivity(intent);
     }
 }
