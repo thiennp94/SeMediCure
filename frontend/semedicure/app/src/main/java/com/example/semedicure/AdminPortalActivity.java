2983414b1e3c5a445c67558f70669fe2e5facfb0
ptnguyen <ptnguyen@iastate.edu>
2022-04-07 21:00:01 -0500

    minor update Admin Portal Activity

--- frontend/semedicure/app/src/main/java/com/example/semedicure/AdminPortalActivity.java
+++ frontend/semedicure/app/src/main/java/com/example/semedicure/AdminPortalActivity.java
@@ -2,7 +2,9 @@ package com.example.semedicure;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AdminPortalActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class AdminPortalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_portal);
+    }
+
+    public void approveAppointment(View view) {
+        Intent intent = new Intent(this, ApproveApptActivity.class);
+        startActivity(intent);
+    }
+
+    public void checkIn(View view) {
+        Intent intent = new Intent(this, CheckInActivity.class);
+        startActivity(intent);
+    }
+
+    public void updateProfile(View view) {
+        Intent intent = new Intent(this, AdminInfoActivity.class);
+        startActivity(intent);
     }
 }
