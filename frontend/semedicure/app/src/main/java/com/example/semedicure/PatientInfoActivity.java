d52ad346a0d77504284d61453225ebba84fceeeb
ptnguyen <ptnguyen@iastate.edu>
2022-05-05 11:39:45 -0500

    Frontend: Update patient data forwarding

--- frontend/semedicure/app/src/main/java/com/example/semedicure/PatientInfoActivity.java
+++ frontend/semedicure/app/src/main/java/com/example/semedicure/PatientInfoActivity.java
@@ -2,6 +2,7 @@ package com.example.semedicure;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -27,7 +28,7 @@ import java.util.regex.Pattern;
 
 /**
  * The patient information activity of the app.
- * The entry point/welcome screen > Login > Patient Portal > Patient Info
+ * The entry point/welcome screen > Login > Patient Portal > Patient Profile > Patient Info
  */
 public class PatientInfoActivity extends AppCompatActivity {
 
@@ -83,7 +84,7 @@ public class PatientInfoActivity extends AppCompatActivity {
         mTextViewSSN = findViewById(R.id.txtSSN);
         mTextViewInsurer = findViewById(R.id.txtInsurer);
         mTextViewPolicyHolder = findViewById(R.id.txtPolicyHolder);
-        mTextViewGroupNum = findViewById(R.id.txtGroupNum);
+//        mTextViewGroupNum = findViewById(R.id.txtGroupNum);
 //        Button buttonCreate = findViewById(R.id.buttonUpdate);
 
         mQueue = Volley.newRequestQueue(this);
@@ -94,6 +95,33 @@ public class PatientInfoActivity extends AppCompatActivity {
 //                jsonParse();
 //            }
 //        });
+
+        // Get data from Patient Profile screen
+        // if NULL => new account
+        Intent intent = getIntent();
+        String value1 = intent.getStringExtra("Key_1");
+        int value2 = intent.getIntExtra("Key_2", 0);
+        boolean value3 = intent.getBooleanExtra("Key_3", false);
+
+        int id = intent.getIntExtra("id", 0);
+        String city = intent.getStringExtra("city");
+        int dateOfBirth = intent.getIntExtra("date_of_birth", 0);
+        String email = intent.getStringExtra("email");
+        String firstName = intent.getStringExtra("firstName");
+        int height = intent.getIntExtra("height", 0);
+        String insurerName = intent.getStringExtra("insurer_name");
+        String lastName = intent.getStringExtra("last_name");
+        String middleName = intent.getStringExtra("middle_name");
+        String password = intent.getStringExtra("password");
+        int phoneNumber = intent.getIntExtra("phone_number", 0);
+        String policyHolderName = intent.getStringExtra("policy_holder_name");
+        String prescriptions = intent.getStringExtra("prescription");
+        int ssn = intent.getIntExtra("ssn", 0);
+        String state = intent.getStringExtra("state");
+        String streetAddress = intent.getStringExtra("street_address");
+        String username = intent.getStringExtra("username");
+        int weight = intent.getIntExtra("weight", 0);
+        int zip = intent.getIntExtra("zip", 0);
     }
 
     public void getPatientInfo(View view){
