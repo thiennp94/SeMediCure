d186e281aebea3fb1a2e9d1525b378b5a62b2824
rpc1 <rpc1@iastate.edu>
2022-05-05 14:11:16 -0500

    scrapping the relationships in the database

--- demo2/src/main/java/coms309/backend/visit/Visit.java
+++ demo2/src/main/java/coms309/backend/visit/Visit.java
@@ -4,10 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import coms309.backend.patient.Patient;
 import coms309.backend.doctor.Doctor;
 
@@ -36,13 +32,6 @@ public class Visit {
      * @OneToOne creates a relation between the current entity/table(Laptop) with the entity/table defined below it(User)
      * @JsonIgnore is to assure that there is no infinite loop while returning either user/laptop objects (laptop->user->laptop->...)
      */
-    @OneToOne
-    @JsonIgnore
-    private Doctor doctor;
-    
-    @OneToOne
-    @JsonIgnore
-    private Patient patient;
 
     public Visit(int id, String docName, String patientName, String reason, int date, Doctor doctor, Patient patient) {
         super();
@@ -51,8 +40,6 @@ public class Visit {
         this.patientName = patientName;
         this.reason = reason;
         this.date = date;
-        this.doctor = doctor;
-        this.patient = patient;
     }
 
     public Visit() {
@@ -107,23 +94,4 @@ public class Visit {
 	public void setDate(int date) {
 		this.date = date;
 	}
-
-	public Doctor getDoctor() {
-		return doctor;
-	}
-
-	public void setDoctor(Doctor doctor) {
-		this.doctor = doctor;
-	}
-
-	public Patient getPatient() {
-		return patient;
-	}
-
-	public void setPatient(Patient patient) {
-		this.patient = patient;
-	}
-
-  
-
 }
